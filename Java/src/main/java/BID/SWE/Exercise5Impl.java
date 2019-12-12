73aa414f98f5bf47f3ba81550952b8217b65c0c5
IAmBlind <50620870+IAmBlind@users.noreply.github.com>
2019-12-12 17:12:31 +0100

    update exercise 5

--- Java/src/main/java/BID/SWE/Exercise5Impl.java
+++ Java/src/main/java/BID/SWE/Exercise5Impl.java
@@ -1,15 +1,36 @@
 package BID.SWE;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
 import java.util.LinkedList;
 
 public class Exercise5Impl implements Exercise5
 {
-    LinkedList<String>myList = new LinkedList();
+   /*LinkedList<String>myList = new LinkedList();
 
     @Override
     public Object Method1(String s, int i) {
         myList.add(s);
         myList.add(Integer.toString(i));
+        return myList;
+    }*/
+    @Override
+    public LinkedList<String> Method1(String s, int i) {
+        LinkedList<String> myList = new LinkedList<>();
+        try {
+            Socket socket = new Socket(s, i);
+            InputStreamReader inputReader = new InputStreamReader(socket.getInputStream());
+            BufferedReader bufferedReader = new BufferedReader(inputReader);
+
+            String newline;
+            while((newline = bufferedReader.readLine()) != null) {
+                myList.add(newline);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         return myList;
     }
 }
